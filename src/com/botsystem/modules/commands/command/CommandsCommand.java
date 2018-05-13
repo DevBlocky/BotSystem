93b48e89a795c1818b1be3bfe41a4a189112c133
BlockBa5her <blocckba5her@gmail.com>
2018-05-12 21:40:42 -0700

    update CommandsCommand for new interface

--- src/com/botsystem/modules/commands/command/CommandsCommand.java
+++ src/com/botsystem/modules/commands/command/CommandsCommand.java
@@ -1,9 +1,15 @@
 package com.botsystem.modules.commands.command;
 
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 import com.botsystem.extensions.BotSystemEmbed;
 import com.botsystem.modules.commands.BotCommandsModule;
 import com.botsystem.modules.commands.BotCommand;
 import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.MessageEmbed;
 
 public class CommandsCommand extends BotCommand {
     private String reqPerm;
@@ -13,21 +19,42 @@ public class CommandsCommand extends BotCommand {
         this.reqPerm = reqPerm;
         this.cmd = cmd;
     }
+    
+    private Map<String, List<BotCommand>> sortCmdsByReqPerm() {
+    	BotCommand[] cmds = parent.getCommands();
+    	Map<String, List<BotCommand>> tmp = new LinkedHashMap<>();
+    	
+    	for (BotCommand cmd : cmds) {
+    		String reqPerm = cmd.getRequiredPerm();
+    		if (!tmp.containsKey(reqPerm))
+    			tmp.put(reqPerm, new LinkedList<>());
+    		
+    		tmp.get(reqPerm).add(cmd);
+    	}
+    	
+    	return tmp;
+    }
 
     @Override
     public void onInvoke(Message m, String[] args) {
-        BotCommand[] commands = parent.getCommands();
+        Map<String, List<BotCommand>> sort = sortCmdsByReqPerm();
 
         BotSystemEmbed emb = new BotSystemEmbed();
-
-        String output = "";
-        for (BotCommand c : commands) {
-            output += "`" + BotCommandsModule.HEADER + c.getCmd() + "` - " + c.getDesc() + "\n";
+        
+        for (String key : sort.keySet()) {
+        	String perm = Character.toUpperCase(key.charAt(0)) + key.substring(1, key.length()); // capitalize first letter
+        	StringBuilder permCmdOutput = new StringBuilder(); // output variable
+        	
+        	for (BotCommand cmd : sort.get(key)) {
+        		permCmdOutput.append("`" + BotCommandsModule.HEADER + cmd.getCmd() + "` - " + cmd.getDesc() + "\n");
+        	}
+        	
+        	emb.addField(new MessageEmbed.Field(perm + " Commands", permCmdOutput.toString(), false));
         }
 
-        emb.setTitle("BotSystem Commands");
-        emb.setDescription(output);
-
+        if (emb.isEmpty())
+        	emb.setTitle("BotSystem Commands"); // making sure of no crash on build
+        
         m.getChannel().sendMessage(emb.build()).queue();
     }
 
